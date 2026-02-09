c7bf4f1043d25a3d84aed448cf4d483f48f3094e
SaDang <dudrl0401@naver.com>
2026-02-09 09:59:50 +0900

    [2026. 02. 09.]  - 첫 커밋

---
+++ src/main/java/com/daedong/zipmap/ZipmapApplication.java
@@ -0,0 +1,13 @@
+package com.daedong.zipmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipmapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daedong/zipmap/ZipmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daedong.zipmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
