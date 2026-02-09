0aa990c3e06a5cd2e62dd5763f63f81ac93f376a
SaDang <dudrl0401@naver.com>
2026-02-09 14:57:40 +0900

    .

--- src/main/java/com/daedong/zipmap/ZipmapApplication.java
+++ src/main/java/com/daedong/zipmap/ZipmapApplication.java
@@ -1,11 +1,10 @@
 package com.daedong.zipmap;
 
-import org.mybatis.spring.annotation.MapperScan;import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZipmapApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ZipmapApplication.class, args);
     }
