09f3c7af7b34b3eb510a5b1a247dbd74f5acd288
sh <limsh1575@gmail.com>
2026-02-09 17:08:52 +0900

    미완성

--- src/main/java/com/daedong/zipmap/controller/ReviewController.java
+++ src/main/java/com/daedong/zipmap/controller/ReviewController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/reviews")
+@RequestMapping("/review")
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
