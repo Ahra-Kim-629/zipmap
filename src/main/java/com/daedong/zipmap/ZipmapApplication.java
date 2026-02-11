897e0fb425e92d32ead878b44e1b8cbf1afb3a19
sh <limsh1575@gmail.com>
2026-02-11 18:24:26 +0900

    오류

--- src/main/java/com/daedong/zipmap/ZipmapApplication.java
+++ src/main/java/com/daedong/zipmap/ZipmapApplication.java
@@ -1,9 +1,11 @@
 package com.daedong.zipmap;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.daedong.zipmap.mapper")
 public class ZipmapApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZipmapApplication.class, args);
