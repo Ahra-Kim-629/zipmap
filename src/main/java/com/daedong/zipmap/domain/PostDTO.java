bb5d61b3baf0405b936391b84696faf9a9889002
SaDang <dudrl0401@naver.com>
2026-02-20 14:30:42 +0900

    커밋

--- src/main/java/com/daedong/zipmap/domain/PostDTO.java
+++ src/main/java/com/daedong/zipmap/domain/PostDTO.java
@@ -12,17 +12,16 @@ public class PostDTO {
     private String title;
     private String content;
     private Long userId;
-    private String loginId; // 로그인 아이디로 커뮤니티 글에 나오게 하기 위해 추가
     private String category;
     private String location;
+    private Long likeCount;
+    private Long viewCount;
     private String postStatus;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
+    private String loginId; // 로그인 아이디로 커뮤니티 글에 나오게 하기 위해 추가
+
     private List<File> fileList;
     private List<Reply> replyList;
-
-    private int likeCount;
-    private int dislikeCount;
-    private int viewCount;
 }
